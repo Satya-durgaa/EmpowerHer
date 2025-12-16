e1dd77e73fdf2ae00a22e68d7b0f3f52aa6d9ee4
Satya Durga Madepalli <satyadurgamadepalli@gmail.com>
2025-12-16 21:50:38 +0530

    Add files via upload

---
+++ vite-react-props-drilling/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Component1 from './Component1'
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Props Drilling Example</h2>
+      <Component1 />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-react-video-player/src/App.jsx
@@ -0,0 +1,13 @@
+
+import VideoPlayer from './VideoPlayer'
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Video Player using useRef</h2>
+      <VideoPlayer />
+    </div>
+  )
+}
+
+export default App
